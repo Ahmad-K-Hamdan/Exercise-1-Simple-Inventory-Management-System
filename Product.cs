7e9f43b850b3448a6fcbc8b05f9baad2d892733e
Ahmad-K-Hamdan <Ahmadkhamdan@outlook.com>
2025-07-24 03:20:18 +0300

    Created the Product class with the required properties and ToString method. Additionally, wrote some testing lines in the main method.

---
+++ Product.cs
@@ -0,0 +1,56 @@
+﻿class Product
+{
+    public Product(string name, float price, int quantity)
+    {
+        Name = name;
+        Price = price;
+        Quantity = quantity;
+    }
+
+    private string _name;
+    public string Name
+    {
+        get => _name;
+        set
+        {
+            value = value.Trim();
+            if (value.All(Char.IsLetter))
+                _name = value;
+            else
+                Console.WriteLine($"Error! Input: {value}. Name can only contain characters");
+        }
+    }
+
+
+    private float _price;
+    public float Price
+    {
+        get => _price;
+        set
+        {
+            if (value > 0)
+                _price = value;
+            else
+                Console.WriteLine($"Error! Input: {value}. Price cannot be non-positive");
+        }
+    }
+
+
+    private int _quantity;
+    public int Quantity
+    {
+        get => _quantity;
+        set
+        {
+            if (value >= 0)
+                _quantity = value;
+            else
+                Console.WriteLine($"Error! Input: {value}. Quantity cannot be negative");
+        }
+    }
+
+    public override string ToString()
+    {
+        return "Name = " + Name + ", Price = " + Price + ", Quantity = " + Quantity;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿class Program
+{
+    public static void Main()
+    {
+        Product myProduct = new Product("apples", 12.1f, 50);
+        Console.WriteLine(myProduct.ToString());
+        myProduct.Name = "bananas41";
+        myProduct.Price = 20f;
+        myProduct.Quantity = -3;
+        Console.WriteLine(myProduct.ToString());
+    }
+}
